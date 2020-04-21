bb75eb58a8379e1a4be3e662d0e54e39dcd16e14
Cristiano <neivacristiano@yahoo.com.br>
2020-04-21 11:26:17 -0300

    adding routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Switch, BrowserRouter} from 'react-router-dom'
 import Home from './pages/home'
 import Header from './components/header'
+import Erro from './components/erro'
 
 export const Routes = () => {
     return (
@@ -9,6 +10,7 @@ export const Routes = () => {
             <Header></Header>
             <Switch>
                 <Route exact path='/' component={Home}></Route>
+                <Route path='*' component={Erro}></Route>
             </Switch>
         </BrowserRouter>
     )
